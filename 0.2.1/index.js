66d7677a3b0c93a3eda2cf6e42e4296f8c2098ad
PauloGabriel347 <83320713+PauloGabriel347@users.noreply.github.com>
2022-09-16 20:57:14 -0300

    1 file modified, 2 added, 1 deleted

---
+++ 0.2.1/index.js
@@ -0,0 +1,22 @@
+const filmes = [
+  {
+    nome: 'As Loucas Aventuras de James West',
+    ano: 1999,
+    elenco: ['Will Smith', 'Salma Hayek'],
+    genero: 'Comedia',
+  },
+  {
+    nome: 'MoonWalker',
+    ano: 1988,
+    elenco: ['Michael Jackson', 'Joe Pesci'],
+    genero: 'Musical',
+  },
+  {
+    nome: 'A Mumia',
+    ano: 1999,
+    elenco: ['Brendan Fraser', 'Rachel Weisz', 'Arnold Voslo'],
+    genero: 'Acao',
+  },
+];
+
+console.log(filmes);
